04ba9233691f416c93f51be7bf0d04178bdf8398
Shoxista Iriskulova <shoxistairiskulova@gmail.com>
2025-07-25 14:23:03 +0500

    BaseModel ready for extension

---
+++ apps/common/models.py
@@ -0,0 +1,15 @@
+import uuid
+from django.db import models
+
+
+class BaseModel(models.Model):
+    guid = models.UUIDField(
+        unique=True, default=uuid.uuid4, editable=False, db_index=True
+    )
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
