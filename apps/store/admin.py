db90baff89029de1af56d93f0f546fb8ae08a8ef
Shoxista Iriskulova <shoxistairiskulova@gmail.com>
2025-08-13 02:25:18 +0500

    feat(store-admin): add Category model to admin panel

--- apps/store/admin.py
+++ apps/store/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
+
+from .models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(TranslationAdmin):
+    list_display = ("id", "name", "parent")
+    search_fields = ("parent",)
